cc2958a245a379e1062c3cac93473018fde06e40
Fern-the-ghost <W1907532@apps.losrios.edu>
2025-05-12 19:59:43 -0700

    Update Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -63,11 +63,12 @@ The algorithm is as follows:
 Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) : m_A(2, numPoints) { //initialize m_A
     m_ttl = TTL;
     m_numPoints = numPoints;
-    float randomNumber = static_cast<float>(rand()) / (RAND_MAX);
-    m_radiansPerSec = randomNumber * M_PI;
+    float randomNumber = static_cast<float>(rand()) / (RAND_MAX);       
+
+    m_radiansPerSec = static_cast<float>(randomNumber * M_PI);
 
     m_cartesianPlane.setCenter(0, 0);
-    m_cartesianPlane.setSize(target.getSize().x, -1.0 * target.getSize().y);
+    m_cartesianPlane.setSize(target.getSize().x, -1.0 * target.getSize().y);           
     m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
 
     m_vx = rand() % 401 + 100; //Can be between whatever number (100-500)
@@ -82,9 +83,9 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 
     for (int j = 0; j < numPoints; j++) {
         r = static_cast<float>(rand() % 21 + 20); //20-40 (61 + 20) for 20-80
-        dx = r * cos(theta);
-        dy = r * sin(theta);
-        m_A(0, j) = m_centerCoordinate.x + dx;
+        dx = static_cast<float>(r * cos(theta));            //PROBLEM, fixed to static: dx, dy & r is float, cos(theta)/sin(theta) returns double
+        dy = static_cast<float>(r * sin(theta));
+        m_A(0, j) = m_centerCoordinate.x + dx;      
         m_A(1, j) = m_centerCoordinate.y + dy;
         theta += dTheta;
     }
@@ -123,13 +124,14 @@ When the loop is finished, draw the VertexArray:
 
 void Particle::draw(RenderTarget& target, RenderStates states) const {
     VertexArray lines(TriangleFan, m_numPoints + 1);
+ 
     Vector2f center(target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane));
-
     lines[0].position = center;
     lines[0].color = m_color1;
 
     for (int j = 1; j <= m_numPoints; j++) {
-        lines[j].position = (Vector2f)target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane);
+        lines[j].position = static_cast<Vector2f>(target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane)); //static_cast
+
         lines[j].color = m_color2;
     }
     target.draw(lines, states);
@@ -177,10 +179,8 @@ Update the particle's center coordinate:
  */
 
 void Particle::translate(double xShift, double yShift) {
-    int nCols = m_A.getCols();
-    TranslationMatrix T(xShift, yShift, nCols);
-    m_A = T + m_A;
-    //T += m_A;
+    TranslationMatrix T(xShift, yShift, m_A.getCols());
+    m_A = m_A + T;
     m_centerCoordinate.x += static_cast<float>(xShift);
     m_centerCoordinate.y += static_cast<float>(yShift);
 }
@@ -202,7 +202,7 @@ Shift our particle back to its original center:
 
 void Particle::rotate(double theta) {
     Vector2f temp = m_centerCoordinate;
-    translate(-temp.x, -temp.y);
+    translate(-temp.x, -temp.y);            
     RotationMatrix R(theta);
     m_A = R * m_A;
     translate(temp.x, temp.y);
